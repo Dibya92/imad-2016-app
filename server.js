3f96fb9be535356a2bcfb205180d553a4218ea22
Dibya92 <dibyadhar524@gmail.com>
2016-11-10 14:59:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var counter = 0;
+app.get('/counter',function(req,res){
+   counter = counter + 1;
+   res.send(counter.toString());
 });
 
 app.get('/article-two',function(req,res){

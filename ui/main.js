8537d14c26338b263e87c15f3d178ea61acb7110
Dibya92 <dibyadhar524@gmail.com>
2016-11-27 16:24:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+//code to locally update the counter
+
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function() {
+
+//Render the variable in the span loaction
+
+   counter = counter + 1;
+
+   //Select the correct span area(i.e by id) from the html document
+   
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+
+}

0ee8be47d9b774084dc397c21dc88458bea8ee28
Dibya92 <dibyadhar524@gmail.com>
2016-11-30 00:41:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,44 @@
 console.log('Loaded!');
-//code to locally update the counter
+//Counter code
 
 var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function() {
 
-//Render the variable in the span loaction
+  // create a request object named "request"
+  
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  
+  request.onreadystatechange = function(){
+        // process the server response
+          
+        if (request.readyState === XMLHttpRequest.DONE) {
+               // everything is good, the response is received
+               
+               if (request.status === 200) {
+                    // Perfect! Now we want to capture the value from the respone
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
 
-   counter = counter + 1;
+                                
+                } 
+             
+             
+        } 
+             
+  //Make the request:
+  
+  request.open('GET','http://dibya92.imad.hasura-app.io/counter',true);
+  request.send(null);
+      
+  };
+  
 
    //Select the correct span area(i.e by id) from the html document
    
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-
+   
 }

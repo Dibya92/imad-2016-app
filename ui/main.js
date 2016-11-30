c0ef717e4dbb6c14db6726f5a77b65e81736898e
Dibya92 <dibyadhar524@gmail.com>
2016-11-30 11:38:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,5 +39,28 @@ button.onclick = function() {
  
 };
    
-   
+//Submit Name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('subBtn');
+submit.onclick = function(){
+    
+ //Make a request to the server and send the name
+
+
+ //Capture a list of names and render it as list
+   var names = ['name1','name2','name3'];
+   var list = '';
+ 
+   for (var i = 0;i < names.length;i++){
+     
+              list += '<li>' + names[i] + '</li>';
+    }
+
+    var ul = document.getElementById('nameList');
+
+    
+};
+
    

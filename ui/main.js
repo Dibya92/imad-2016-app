2a3b965b57500e4e0be0b820206705716208a1fc
Dibya92 <dibyadhar524@gmail.com>
2016-11-30 13:18:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
-//Counter code
+
+// 1.Counter code
 
 var button = document.getElementById('counter');
 
@@ -31,7 +32,8 @@ button.onclick = function() {
         } 
   
   };           
-  //Make the request:
+ 
+ // Make the request:
   
   request.open('GET','http://dibya92.imad.hasura-app.io/counter',true);
   request.send(null);
@@ -39,27 +41,61 @@ button.onclick = function() {
  
 };
    
-//Submit Name
+// 2.Name Submit code
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('subBtn');
+
 submit.onclick = function(){
-    
- //Make a request to the server and send the name
-
-
- //Capture a list of names and render it as list
-   var names = ['name1','name2','name3'];
-   var list = '';
+  
+   //+ create a request object named "request"
+  
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  
+  request.onreadystatechange = function(){
+        // process the server response
+          
+        if (request.readyState === XMLHttpRequest.DONE) {
+               // everything is good, the response is received
+               
+               if (request.status === 200) {
+                    
+                        //Capture a list of names and render it as list
+                          var names = request.responseText;//The response obtain from the server will be a JS String, need to convert back                                to array
+                          names = JSON.parse(names);
+                          var list = '';
  
-   for (var i = 0;i < names.length;i++){
+                          for (var i = 0;i < names.length;i++){
      
-              list += '<li>' + names[i] + '</li>';
-    }
+                                     list += '<li>' + names[i] + '</li>';
+                          }
 
-    var ul = document.getElementById('nameList');
-    ul.innerHTML = list;
+                          var ul = document.getElementById('nameList');
+                          ul.innerHTML = list;
+
+                                
+                } 
+             
+             
+        } 
+  
+   };           
+
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;//Vale entered into the input box in the html document
+
+ 
+ 
+  //Make the request to server:
+  
+  request.open('GET','http://dibya92.imad.hasura-app.io/submit-name?name='+name,true);
+  request.send(null);
+       
+ //+Make a request to the server and send the name
+
+
 
     
 };

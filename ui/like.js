98d9ea09edcc335b528e4519715b8a35fb2d9204
Dibya92 <dibyadhar524@gmail.com>
2016-12-02 01:23:51 +0530

    [imad-console] Updates ui/like.js

--- ui/like.js
+++ ui/like.js
@@ -39,6 +39,7 @@ button1.onclick = function() {
  
 };
 
+
 // code for second like button:
 var button2 = document.getElementById('bttnTwo');
 
@@ -73,6 +74,81 @@ button2.onclick = function() {
  // Making the request:
   
   request.open('GET','http://dibya92.imad.hasura-app.io/counter2',true);
+  request.send(null);
+      
+ };
+// Code for second like button:
+var button2 = document.getElementById('bttnTwo');
+
+button2.onclick = function() {
+
+  // create a request object named "request"
+  
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  
+  request.onreadystatechange = function(){
+        // process the server response
+          
+        if (request.readyState === XMLHttpRequest.DONE) {
+               // everything is good, the response is received
+               
+               if (request.status === 200) {
+                    // Perfect! Now we want to capture the value from the respone
+                    var counter = request.responseText;
+                    var span = document.getElementById('count2');
+                    span.innerHTML = counter.toString();
+
+                                
+                } 
+             
+             
+        } 
+  
+  };           
+ 
+ // Making the request:
+  
+  request.open('GET','http://dibya92.imad.hasura-app.io/counter2',true);
+  request.send(null);
+      
+ };
+
+// Code for third like button:
+var button3 = document.getElementById('bttnThree');
+
+button3.onclick = function() {
+
+  // create a request object named "request"
+  
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  
+  request.onreadystatechange = function(){
+        // process the server response
+          
+        if (request.readyState === XMLHttpRequest.DONE) {
+               // everything is good, the response is received
+               
+               if (request.status === 200) {
+                    // Perfect! Now we want to capture the value from the respone and put into the correct span location within the doc.
+                    var counter = request.responseText;
+                    var span = document.getElementById('count3');
+                    span.innerHTML = counter.toString();
+
+                                
+                } 
+             
+             
+        } 
+  
+  };           
+ 
+ // Making the request:
+  
+  request.open('GET','http://dibya92.imad.hasura-app.io/counter3',true);
   request.send(null);
       
  

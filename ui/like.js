2c435843cab4211d3306b64f98dcf0220aacdc7c
Dibya92 <dibyadhar524@gmail.com>
2016-12-01 20:21:23 +0530

    [imad-console] Updates ui/like.js

--- ui/like.js
+++ ui/like.js
@@ -1,1 +1,41 @@
 //code for counting like in the home page
+// 1.Counter code
+
+var button = document.getElementById('counter');
+
+
+button.onclick = function() {
+
+  // create a request object named "request"
+  
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  
+  request.onreadystatechange = function(){
+        // process the server response
+          
+        if (request.readyState === XMLHttpRequest.DONE) {
+               // everything is good, the response is received
+               
+               if (request.status === 200) {
+                    // Perfect! Now we want to capture the value from the respone
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+
+                                
+                } 
+             
+             
+        } 
+  
+  };           
+ 
+ // Make the request:
+  
+  request.open('GET','http://dibya92.imad.hasura-app.io/counter',true);
+  request.send(null);
+      
+ 
+};

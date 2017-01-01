655ed5b53c0064c2868a4c1febd4ec4b2d23a925
Dibya Dhar <dibyadhar524@gmail.com>
2017-01-01 21:53:18 +0530

    [imad-console] Updates ui/comment.js

--- ui/comment.js
+++ ui/comment.js
@@ -1,1 +1,22 @@
+//Select and capure the name from the document
 
+var comment = document.getElementById("comment").value;
+var submit = document.getElementById("commentButton");
+
+submit.onclick = function() {
+    
+    //To make a request to the server & send the comment
+   
+   
+   
+    //Render the list from the server
+       //Templating Job
+       var comments = ["comment1","comment2","comment3"];
+       var list = "";  //Empty String
+       for (var i=0; i<=comments.length; i++) {
+           list += "<li>" + comments[i] + "</li>" + "<br />";
+         }
+       var commentList = document.getElementById("commentList");
+       commentList.innerHTML = list;
+    
+};
